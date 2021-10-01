d320c6242476b4827848820e046a31dcf9b16862
Madhurima Ghosh <54733636+Madhurima-Ghosh@users.noreply.github.com>
2021-10-01 14:50:01 +0530

    Create swap_xor.c

---
+++ c/swap_xor.c
@@ -0,0 +1,20 @@
+//Swapping 2 numbers without using 3rd variable
+//using XOR bitwise operator
+
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter first number:");
+ scanf("%d",&a);
+ printf("Enter second number:");
+ scanf("%d",&b);
+ printf("\nBefore swapping:%d",a);
+ printf("\nBefore swapping:%d",b);
+ a=a^b;
+ b=a^b;
+ a=a^b;
+ printf("\n\nAfter swapping:%d",a);
+ printf("\nAfter swapping:%d",b);
+ return 0;
+ }
